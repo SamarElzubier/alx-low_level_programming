04080fb99bbbe54c61587f3dfa8ce1f401d5d9ce
samarelzubair <samarmaruod@gmail.com>
2023-08-12 04:22:17 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 
 
 /**
-* main - prints "with proper grammar, but the outcome is
-* a piece of art, followed by a new line."
+* main - Entry point
+* 
 *Return: Always 0 (Success) 
 */
-
 int main(void)
 
 {
