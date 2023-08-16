dbb59ec5a9294cf067e5218f362151de6d7cd0c1
samarelzubair <samarmaruod@gmail.com>
2023-08-16 09:56:08 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,7 +21,7 @@ fib1_half1 = fib1 / 10000000000;
 fib2_half1 = fib2 / 10000000000;
 fib1_half2 = fib1 % 10000000000;
 fib2_half2 = fib2 % 10000000000;
-for (count 93; count < 99; count++)
+for (count = 93; count < 99; count++)
 {
 half1 = fib1_half1 + fib2_half1;
 half2 = fib1_half2 + fib2_half2;
@@ -38,6 +38,6 @@ fib1_half2 = fib2_half2;
 fib2_half1 = half1;
 fib2_half2 = half2;
 }
-printf("\n")
+printf("\n");
 return (0);
 }
