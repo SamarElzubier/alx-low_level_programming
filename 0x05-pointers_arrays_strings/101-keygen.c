61b6d47c745d0d54a10d8dc55d0a26e74c0e45d2
samarelzubair <samarmaruod@gmail.com>
2023-08-21 02:28:31 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 char password[84];
-int index = 0, sum = 0, diff_half1,diff_half2;
+int index = 0, sum = 0, diff_half1, diff_half2;
 srand(time(0));
 while (sum < 2772)
 {
@@ -21,7 +21,7 @@ if (sum != 2772)
 {
 diff_half1 = (sum - 2772) / 2;
 diff_half2 = (sum - 2772) / 2;
-if ((sum - 2772) % 2 !=0)
+if ((sum - 2772) % 2 != 0)
 diff_half1++;
 for (index = 0; password[index]; index++)
 {
@@ -30,9 +30,11 @@ if (password[index] >= (33 + diff_half1))
 password[index] -= diff_half1;
 break;
 }
+}
 for (index = 0; password[index]; index++)
 {
 if (password[index] >= (33 + diff_half2))
+{
 password[index] -= diff_half2;
 break;
 }
