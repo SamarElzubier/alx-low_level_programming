4f656e03db341181188441b7077023293e920703
samarelzubair <samarmaruod@gmail.com>
2023-08-21 02:06:15 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-char password(84);
+char password[84];
 int index = 0, sum = 0, diff_half1,diff_half2;
 srand(time(0));
 while (sum < 2772)
@@ -38,6 +38,6 @@ break;
 }
 }
 }
-printf(%s", password);
+printf("%s", password);
 return (0);
 }
